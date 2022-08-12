814d94101a2c7915caf46ce2be24b31d946afaaf
kaibalya jena <kaibalya18j@gmail.com>
2022-08-12 16:55:52 +0530

    images

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_login/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.deepPurple,
       ),
-      home: Scaffold(),
+      home: LoginPage(),
     );
   }
 }
