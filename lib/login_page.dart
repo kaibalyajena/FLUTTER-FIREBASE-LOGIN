1b188320379b271b3d44eaced57260ffc1d51bf8
kaibalya jena <kaibalya18j@gmail.com>
2022-08-12 17:18:53 +0530

    image in container through decoration

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,6 +10,19 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(children: [
+        Container(
+          width: w,
+          height: h * 0.3,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("img/loginimg.png"), fit: BoxFit.cover)),
+        ),
+      ]),
+    );
   }
 }
